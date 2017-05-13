710b736de32edb191681c9d40165fb722c232c0a
Phelan Lewis <phelanlewis@gmail.com>
2017-05-13 10:34:49 -0700

    background colors added

--- App.js
+++ App.js
@@ -6,26 +6,48 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity
-          style={{ padding: 5, borderRadius: 5 }}
-          onPress={() => Alert.alert('Happy Mothers Day', 'From Up on the Mountain')}
-        >
+          style={{
+            padding: 5,
+            borderWidth: 5,
+            borderColor: 'red',
+            borderRadius: 20,
+            backgroundColor: '#444'
+          }}
+          onPress={
+            () => Alert.alert('Happy Mothers Day', 'From Up on the Mountain, Love Phelan')
+          }>
           <Image
             source={{uri: 'https://s-media-cache-ak0.pinimg.com/236x/b1/5e/12/b15e12194f2e52530ab9f058f84dbf25.jpg'}}
             style={{
                     width: 200,
                     height: 200,
-                    marginBottom: 10,
+                    padding: 10,
+                    margin: 10,
                     borderWidth: 5,
                     borderColor: 'red',
-                    borderRadius: 10
+                    borderRadius: 20,
                   }}
           />
         </TouchableOpacity>
         <Image
           source={{uri: 'https://img.clipartfest.com/b194ddf2f9b1a6ba56619da460e83570_red-arrows-set-up-clip-art-symbols-arrow-pointing-up-clipart_450-592.png'}}
-          style={{width: 100, height: 130, marginTop: 10, marginBottom: 10}}
+          style={{
+            width: 100,
+            height: 135,
+            margin: 10,
+          }}
         />
-        <Text style={{ padding: 10, borderRadius: 10, fontSize: 30, backgroundColor: '#444', color: '#fff' }}>Click on Me!</Text>
+        <Text style={{
+          padding: 20,
+          overflow: 'hidden',
+          borderRadius: 20,
+          borderWidth: 5,
+          fontSize: 30,
+          backgroundColor: '#444',
+          color: '#fff'
+        }}>
+          Click on Me!
+        </Text>
       </View>
     );
   }
