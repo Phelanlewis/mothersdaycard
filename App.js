cdc417938c720276600e79e419b4a09ac5a479d7
Phelan Lewis <phelanlewis@gmail.com>
2017-05-12 22:45:44 -0700

    added more styling

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity
+          style={{ padding: 5, borderRadius: 5 }}
           onPress={() => Alert.alert('Happy Mothers Day', 'From Up on the Mountain')}
         >
           <Image
@@ -16,15 +17,15 @@ export default class App extends React.Component {
                     marginBottom: 10,
                     borderWidth: 5,
                     borderColor: 'red',
-                    borderRadius: 5
+                    borderRadius: 10
                   }}
           />
         </TouchableOpacity>
         <Image
           source={{uri: 'https://img.clipartfest.com/b194ddf2f9b1a6ba56619da460e83570_red-arrows-set-up-clip-art-symbols-arrow-pointing-up-clipart_450-592.png'}}
-          style={{width: 100, height: 130, marginTop: 10}}
+          style={{width: 100, height: 130, marginTop: 10, marginBottom: 10}}
         />
-        <Text style={{ fontSize: 30 }}>Click on Me!</Text>
+        <Text style={{ padding: 10, borderRadius: 10, fontSize: 30, backgroundColor: '#444', color: '#fff' }}>Click on Me!</Text>
       </View>
     );
   }
