75539dfb2e795df21540d42195cc585f0fd0b3ba
Phelan Lewis <phelanlewis@gmail.com>
2017-05-13 10:37:56 -0700

    removed unused image

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import {
+        StyleSheet,
+        Text,
+        View,
+        Image,
+        TouchableOpacity,
+        Alert
+      }  from 'react-native';
 
 export default class App extends React.Component {
   render() {
